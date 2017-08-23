1f5ea52227a76ce0474ef2baf3a893378e8a8334
Ben Schuiling <benjaminschuiling@smarttech.com>
2017-08-23 14:36:55 -0700

    importing node_modules from vendor tree as expected by ember-cli

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 /* eslint-env node */
 'use strict';
 
+let path = require('path');
+let mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-js-mock',
 
+  treeForVendor: function(tree) {
+    let jsMockAssets = path.join(this.project.nodeModulesPath, 'js-mock', 'dist');
+
+    tree = (tree) ? mergeTrees([ tree, jsMockAssets ]) : jsMockAssets;
+
+    return tree;
+  },
+
   included: function (app) {
     if (app.tests) {
-      app.import(this.project.nodeModulesPath + '/js-mock/dist/js-mock.js', {
+      app.import('vendor/js-mock.js', {
         type: 'test'
       });
     }
